0c6155e926faf334d351e731d89076842ad31987
sethvincent <sethvincent@gmail.com>
2013-04-05 13:03:51 -0700

    add spin.js

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
 
+  var opts = {
+    lines: 13, // The number of lines to draw
+    length: 3, // The length of each line
+    width: 2, // The line thickness
+    radius: 6, // The radius of the inner circle
+    corners: 0.5, // Corner roundness (0..1)
+    rotate: 0, // The rotation offset
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    color: '#000', // #rgb or #rrggbb
+    speed: 1.2, // Rounds per second
+    trail: 29, // Afterglow percentage
+    shadow: false, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: 'auto', // Top position relative to parent in px
+    left: 'auto' // Left position relative to parent in px
+  };
+  $(".loading").spin(opts);
+
   var public_spreadsheet_url = 'https://docs.google.com/spreadsheet/pub?key=0AuwSoq4skLXjdGVMWVlrN1Y0elFkYkh1R2xlcDNmd1E&output=html';
 
   Tabletop.init({ 
@@ -9,8 +29,8 @@ $(document).ready(function(){
   });
 
   function thermometer(response, tabletop) {
-    $(".loading").hide();
     $("#thermometer").show();
+    $('.loading').spin(false).hide();
     var data = response[0];
     console.log(data)
     var percentage = ((data.current / data.goal)*100).toFixed(0);
